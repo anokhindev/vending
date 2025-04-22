561ef048a3d9edeb460d8ce6f94ee54966a3a4a9
Анохин Сергей <anokhindev@gmail.com>
2025-04-22 17:27:46 +0300

    linked

---
+++ src/VendorShopLinked.java
@@ -0,0 +1,37 @@
+import products.Product;
+
+import java.util.LinkedList;
+
+public class VendorShopLinked {
+    private LinkedList<Product> products = new LinkedList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product prouct) {
+        products.remove(prouct);
+    }
+
+    public void getProduct(int code, int sum) {
+        for (Product product : products) {
+            if (product.getNumber() == code) {
+
+                if(sum < product.getPrice()) {
+                    System.out.println("не хватает денег");
+                    return;
+                }
+                removeProduct(product);
+                System.out.println(product.getName() + " и сдача - " + (sum - product.getPrice()));
+                break;
+            }
+        }
+    }
+
+
+    public void getAllProducts() {
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+    }
+}
